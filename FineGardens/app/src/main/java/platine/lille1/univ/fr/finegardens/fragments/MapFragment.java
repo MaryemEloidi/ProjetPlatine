714bc41841ef99141cdbd416ec56384e2be28418
Roos Titouan <roost@b11p24.fil.univ-lille1.fr>
2018-02-16 16:00:22 +0100

    Ajout de la redirection vers la page de description

--- FineGardens/app/src/main/java/platine/lille1/univ/fr/finegardens/fragments/MapFragment.java
+++ FineGardens/app/src/main/java/platine/lille1/univ/fr/finegardens/fragments/MapFragment.java
@@ -3,10 +3,11 @@ package platine.lille1.univ.fr.finegardens.fragments;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -25,13 +26,9 @@ import com.google.firebase.database.FirebaseDatabase;
 import platine.lille1.univ.fr.finegardens.R;
 import platine.lille1.univ.fr.finegardens.entities.Jardin;
 
-/**
- * Created by cactus on 16/02/2018.
- */
-
 public class MapFragment extends Fragment implements OnMapReadyCallback {
+
     private GoogleMap mMap;
-    private DatabaseReference mdatabase;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -42,21 +39,13 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         mapFragment.getMapAsync(this);
 
         return view;
-
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mdatabase = FirebaseDatabase.getInstance().getReference().child("Jardins");
+        DatabaseReference mdatabase = FirebaseDatabase.getInstance().getReference().child("Jardins");
 
         LatLng france = new LatLng(46.4892672, 2.7810699);
-
-        /*LatLng lille = new LatLng(50.6397538, 3.0381042999999863);
-        LatLng sydney = new LatLng(50.6373832, 3.1463655999999673);
-        LatLng france = new LatLng(46.4892672, 2.7810699);
-
-        googleMap.addMarker(new MarkerOptions().position(lille).title("Marker in lille"));
-        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));*/
 
         mMap = googleMap;
         mMap.moveCamera(CameraUpdateFactory.zoomTo(6.1f));
@@ -67,24 +56,17 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
                 Jardin jardin = dataSnapshot.getValue(Jardin.class);
 
-
                 // Get recorded latitude and longitude
-
                 long latitude = jardin.getLatitude();
                 long longitude = jardin.getLongitude();
-                String nomJardin = jardin.getNom();
-                String description = jardin.getDescription();
-
 
                 // Create LatLng for each locations
                 LatLng mLatlng = new LatLng(latitude, longitude);
-
 
                 mMap.addMarker(new MarkerOptions()
                         .position(mLatlng)
                         .icon(BitmapDescriptorFactory.fromResource(R.drawable.tree_marker))
                 );
-
             }
 
             @Override
@@ -115,15 +97,16 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
                     @Override
                     public void run() {
-                        mMap.animateCamera(CameraUpdateFactory.
-                                newLatLng(marker.getPosition()));
+                        Fragment descriptionFragment = new DescriptionJardinFragment();
+                        FragmentManager fragmentManager  = getActivity().getSupportFragmentManager();
+                        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                        fragmentTransaction.replace(R.id.flContent, descriptionFragment).commit();
                     }
                 }, 300);
 
                 return false;
             }
         });
-
     }
 }
 
