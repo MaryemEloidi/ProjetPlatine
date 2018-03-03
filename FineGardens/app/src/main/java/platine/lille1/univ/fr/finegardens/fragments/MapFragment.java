eb20410fe2e3572746c818020ce0fa9ca7593b8e
Maryem Eloidi <eloidimaryem@gmail.com>
2018-03-03 18:51:27 +0100

    résolution des conflits

--- FineGardens/app/src/main/java/platine/lille1/univ/fr/finegardens/fragments/MapFragment.java
+++ FineGardens/app/src/main/java/platine/lille1/univ/fr/finegardens/fragments/MapFragment.java
@@ -6,10 +6,7 @@ import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-<<<<<<< HEAD
-import android.util.Log;
-=======
->>>>>>> 714bc41841ef99141cdbd416ec56384e2be28418
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,26 +29,21 @@ import platine.lille1.univ.fr.finegardens.DescriptionJardinActivity;
 import platine.lille1.univ.fr.finegardens.R;
 import platine.lille1.univ.fr.finegardens.entities.Jardin;
 
-<<<<<<< HEAD
+
 import static java.lang.Integer.parseInt;
 
 /**
  * Created by cactus on 16/02/2018.
  */
 
-=======
->>>>>>> 714bc41841ef99141cdbd416ec56384e2be28418
 public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-<<<<<<< HEAD
     private DatabaseReference mdatabase;
     public Jardin jardin;
     public String nomJardin;
     public String des;
-=======
 
->>>>>>> 714bc41841ef99141cdbd416ec56384e2be28418
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -64,20 +56,14 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        DatabaseReference mdatabase = FirebaseDatabase.getInstance().getReference().child("Jardins");
+    public void onMapReady(final GoogleMap googleMap) {
+        mdatabase = FirebaseDatabase.getInstance().getReference().child("Jardins");
 
         LatLng france = new LatLng(46.4892672, 2.7810699);
 
-<<<<<<< HEAD
 
         googleMap.moveCamera(CameraUpdateFactory.zoomTo(6.1f));
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(france));
-=======
-        mMap = googleMap;
-        mMap.moveCamera(CameraUpdateFactory.zoomTo(6.1f));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(france));
->>>>>>> 714bc41841ef99141cdbd416ec56384e2be28418
         mdatabase.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
@@ -85,7 +71,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                 jardin = dataSnapshot.getValue(Jardin.class);
 
                 // Get recorded latitude and longitude
-<<<<<<< HEAD
 
                 double latitude = jardin.getLatitude();
                 double longitude = jardin.getLongitude();
@@ -94,30 +79,20 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                   des = jardin.getDescription();
 
 
-=======
-                long latitude = jardin.getLatitude();
-                long longitude = jardin.getLongitude();
->>>>>>> 714bc41841ef99141cdbd416ec56384e2be28418
 
                 // Create LatLng for each locations
                 LatLng mLatlng = new LatLng(latitude, longitude);
 
-<<<<<<< HEAD
 
                 Marker m = googleMap.addMarker(new MarkerOptions()
-=======
-                mMap.addMarker(new MarkerOptions()
->>>>>>> 714bc41841ef99141cdbd416ec56384e2be28418
                         .position(mLatlng)
+                        .title(nomJardin)
                         .icon(BitmapDescriptorFactory.fromResource(R.drawable.tree_marker))
                         .snippet(des)
 
                 );
-<<<<<<< HEAD
                 m.setTag(jardin);
 
-=======
->>>>>>> 714bc41841ef99141cdbd416ec56384e2be28418
             }
 
             @Override
@@ -141,7 +116,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
             }
         });
 
-        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+        googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
             public boolean onMarkerClick(final Marker marker) {
                 final String nom_jardin = marker.getTitle();
@@ -158,18 +133,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                         i.putExtra("JARDIN-ID", id_jardin);
                        // i.putExtra("JARDIN-DESCRIPTION", des);
 
-<<<<<<< HEAD
                         startActivity(i);
-=======
-                    @Override
-                    public void run() {
-                        Fragment descriptionFragment = new DescriptionJardinFragment();
-                        FragmentManager fragmentManager  = getActivity().getSupportFragmentManager();
-                        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                        fragmentTransaction.replace(R.id.flContent, descriptionFragment).commit();
-                    }
-                }, 300);
->>>>>>> 714bc41841ef99141cdbd416ec56384e2be28418
 
                    // }
                /// }, 0);
@@ -177,10 +141,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
             }
 
         });
-<<<<<<< HEAD
 
-=======
->>>>>>> 714bc41841ef99141cdbd416ec56384e2be28418
     }
 }
 
