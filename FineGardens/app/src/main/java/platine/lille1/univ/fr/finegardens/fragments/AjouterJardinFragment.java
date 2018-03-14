54b9dfdb137193d6a32ac0135e184f1dfaa2608a
roosti <roos.titouan@gmail.com>
2018-03-14 08:25:03 +0100

    creation branche et gestion adresse geolocation

--- FineGardens/app/src/main/java/platine/lille1/univ/fr/finegardens/fragments/AjouterJardinFragment.java
+++ FineGardens/app/src/main/java/platine/lille1/univ/fr/finegardens/fragments/AjouterJardinFragment.java
@@ -1,16 +1,17 @@
 package platine.lille1.univ.fr.finegardens.fragments;
 import platine.lille1.univ.fr.finegardens.*;
 import platine.lille1.univ.fr.finegardens.Controller.JardinController;
-import platine.lille1.univ.fr.finegardens.activities.DescriptionJardinActivity;
 import platine.lille1.univ.fr.finegardens.activities.MainActivity;
 
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.text.TextUtils;
@@ -27,13 +28,12 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 import static android.app.Activity.RESULT_OK;
 import static android.content.Context.LOCATION_SERVICE;
-
-
-/**
- * Created by cactus on 16/02/2018.
- */
 
 public class AjouterJardinFragment extends Fragment{
     private EditText mNomJardinView;
@@ -41,26 +41,26 @@ public class AjouterJardinFragment extends Fragment{
     private EditText mDescriptionView;
     private Button mBtnAjoutJardin;
     private Button mBtnUploadImage;
+    private Button mGeolocButton;
     private TextView mFileName;
     String fileUrl;
     double longitude;
     double latitude;
     public StorageReference mStorage;
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         LocationManager lm = (LocationManager)getActivity().getSystemService(LOCATION_SERVICE);
         if (ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-             longitude = location.getLongitude();
-
-             latitude = location.getLatitude();
-             mStorage = FirebaseStorage.getInstance().getReference();
-
+            longitude = location.getLongitude();
+            latitude = location.getLatitude();
+            mStorage = FirebaseStorage.getInstance().getReference();
         }
+
         return inflater.inflate(R.layout.activity_ajouter_jardin, container, false);
 
     }
@@ -68,7 +68,7 @@ public class AjouterJardinFragment extends Fragment{
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 2 && requestCode == RESULT_OK);
+        //if(requestCode == 2 && requestCode == RESULT_OK);
         final Uri uri = data.getData();
         final ProgressDialog progressDialog = new ProgressDialog(getActivity(),
                 R.style.AppTheme_Custom_dialog);
@@ -106,12 +106,12 @@ public class AjouterJardinFragment extends Fragment{
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mNomJardinView =(EditText)getView().findViewById(R.id.input_nom);
-        mAdresseJardinView = (EditText)getView().findViewById(R.id.input_adresse);
-        mDescriptionView = (EditText)getView().findViewById(R.id.input_description);
-        mBtnAjoutJardin = (Button)getView().findViewById(R.id.btn_ajouter);
-        mBtnUploadImage = (Button)getView().findViewById(R.id.btn_upload);
-        mFileName = (TextView)getView().findViewById(R.id.filename);
+        mNomJardinView = getView().findViewById(R.id.input_nom);
+        mAdresseJardinView = getView().findViewById(R.id.input_adresse);
+        mDescriptionView = getView().findViewById(R.id.input_description);
+        mBtnAjoutJardin = getView().findViewById(R.id.btn_ajouter);
+        mBtnUploadImage = getView().findViewById(R.id.btn_upload);
+        mFileName = getView().findViewById(R.id.filename);
         mBtnUploadImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -163,6 +163,33 @@ public class AjouterJardinFragment extends Fragment{
 
             }
 
+        });
+
+        this.mGeolocButton = getView().findViewById(R.id.geoloc_button);
+        this.mGeolocButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Geocoder geocoder;
+                List<Address> addresses;
+                geocoder = new Geocoder(getContext(), Locale.getDefault());
+
+                try {
+                    addresses = geocoder.getFromLocation(latitude, longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+
+                    String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+                    String city = addresses.get(0).getLocality();
+                    String state = addresses.get(0).getAdminArea();
+                    String country = addresses.get(0).getCountryName();
+                    String postalCode = addresses.get(0).getPostalCode();
+                    String knownName = addresses.get(0).getFeatureName();
+
+                    mAdresseJardinView.setText(address + " " + city + " " + state + " " + country + " " + postalCode + " " + knownName);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getContext(), "ERREUR LOCATION", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
     }
