d44c724f14ca3cef4a6c6390f23546515a764dbe
Roos Titouan <roost@b11p24.fil.univ-lille1.fr>
2018-03-15 10:17:17 +0100

    Changement du floating button et ajout du test SignUp

--- FineGardens/app/src/androidTest/java/platine/lille1/univ/fr/finegardens/LoginActivityEspressoTest.java
+++ FineGardens/app/src/androidTest/java/platine/lille1/univ/fr/finegardens/LoginActivityEspressoTest.java
@@ -1,13 +1,18 @@
 package platine.lille1.univ.fr.finegardens;
 
 import android.content.ComponentName;
+import android.content.Intent;
+import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
 import platine.lille1.univ.fr.finegardens.activities.LoginActivity;
 import platine.lille1.univ.fr.finegardens.activities.MainActivity;
+import platine.lille1.univ.fr.finegardens.activities.SignUpActivity;
 
 import static android.support.test.InstrumentationRegistry.getTargetContext;
 import static android.support.test.espresso.Espresso.onView;
@@ -18,20 +23,39 @@ import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
-/**
- * Created by cactus on 14/03/2018.
- */
-
 public class LoginActivityEspressoTest {
+
     @Rule
-    public ActivityTestRule<LoginActivity> mActivityRule =
-            new ActivityTestRule<>(LoginActivity.class);
+    public ActivityTestRule<LoginActivity> mActivityRule = new ActivityTestRule<>(LoginActivity.class);
+
+    @Before
+    public void setUp() throws Exception{
+        Intents.init();
+    }
+
+    @After
+    public void tearDown(){
+        Intents.release();
+
+    }
+
     @Test
     public void ensureLogin(){
         onView(withId(R.id.input_email)).perform(typeText("test@gmail.com"),closeSoftKeyboard());
         onView(withId(R.id.input_password)).perform(typeText("test12"),closeSoftKeyboard());
         onView(withId(R.id.btn_login)).perform(click());
-        intended(hasComponent(new ComponentName(getTargetContext(),MainActivity.class)));
 
+        mActivityRule.launchActivity(new Intent());
+
+        intended(hasComponent(new ComponentName(getTargetContext(),MainActivity.class)));
+    }
+
+    @Test
+    public void ensureSignUpPageLoading() {
+        onView(withId(R.id.link_signup)).perform(click());
+
+        mActivityRule.launchActivity(new Intent());
+
+        intended(hasComponent(new ComponentName(getTargetContext(), SignUpActivity.class)));
     }
 }
